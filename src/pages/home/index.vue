7d000b0057b604b95e7fd2cac9492d9a5163536b
Silent YANG <silenty4ng@gmail.com>
2025-04-12 13:09:32 +0800

    bug?!

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -3,7 +3,24 @@
     <app-navbar title="首页" :left-arrow="false"></app-navbar>
 
     <text>首页</text>
-
+    <nut-swiper
+      :pagination-visible="true"
+      pagination-color="#426543"
+      pagination-unselected-color="#808080"
+      auto-play="3000">
+      <nut-swiper-item>
+        <img src="https://storage.360buyimg.com/jdc-article/NutUItaro34.jpg" alt="" />
+      </nut-swiper-item>
+      <nut-swiper-item>
+        <img src="https://storage.360buyimg.com/jdc-article/NutUItaro2.jpg" alt="" />
+      </nut-swiper-item>
+      <nut-swiper-item>
+        <img src="https://storage.360buyimg.com/jdc-article/welcomenutui.jpg" alt="" />
+      </nut-swiper-item>
+      <nut-swiper-item>
+        <img src="https://storage.360buyimg.com/jdc-article/fristfabu.jpg" alt="" />
+      </nut-swiper-item>
+    </nut-swiper>
     <app-tabbar active="home"></app-tabbar>
   </app-page>
 </template>
